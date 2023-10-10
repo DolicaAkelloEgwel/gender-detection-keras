5e0757d1a5736a29b4597fe06ef24db4d2631a6d
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2023-10-10 17:48:04 +0100

    fixing imports

--- detect_gender.py
+++ detect_gender.py
@@ -2,20 +2,15 @@
 # website: https://www.arunponnusamy.com
 
 # import necessary packages
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.utils import img_to_array
 from keras.models import load_model
 from keras.utils import get_file
 import numpy as np
-import argparse
 import cv2
 import os
 import cvlib as cv
 
 # handle command line arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-args = ap.parse_args()
 
 # download pre-trained model file (one-time download)
 dwnld_link = "https://github.com/arunponnusamy/cvlib/releases/download/v0.2.0/gender_detection.model"
@@ -23,7 +18,7 @@ model_path = get_file("gender_detection.model", dwnld_link,
                      cache_subdir="pre-trained", cache_dir=os.getcwd())
 
 # read input image
-image = cv2.imread(args.image)
+image = cv2.imread("sample_input.jpg")
 
 if image is None:
     print("Could not read input image")
